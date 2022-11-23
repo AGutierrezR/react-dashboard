1d44ae2add30a3452741962a1d23893ad0c31e75
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2022-11-23 14:48:23 +0100

    Add User Event from Testing Library

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('App', () => {
-  it('should render some text', () => {
+  it('Should render', () => {
     render(<App />)
 
     expect(
@@ -10,5 +11,18 @@ describe('App', () => {
         level: 1,
       })
     ).toHaveTextContent('Vite + React')
+  })
+
+  it('Should update count', async () => {
+    const user = userEvent.setup()
+    render(<App />)
+
+    const countBtn = screen.getByRole('button', {
+      name: /count is 0/i,
+    })
+
+    await user.click(countBtn)
+
+    expect(countBtn).toHaveTextContent('count is 1')
   })
 })
